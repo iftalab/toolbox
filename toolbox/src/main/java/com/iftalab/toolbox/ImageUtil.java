e276bbd7abce7335f81c2a869ef5645d3fd6ec04
ifta <ifta@anyconnect.com>
2019-12-17 17:18:57 +0600

    added bitmapFromVector method

--- toolbox/src/main/java/com/iftalab/toolbox/ImageUtil.java
+++ toolbox/src/main/java/com/iftalab/toolbox/ImageUtil.java
@@ -1,6 +1,13 @@
 package com.iftalab.toolbox;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.widget.ImageView;
+
+import androidx.annotation.DrawableRes;
+import androidx.core.content.ContextCompat;
 
 import com.bumptech.glide.Glide;
 
@@ -10,5 +17,17 @@ public class ImageUtil {
                 .load(imagePath)
                 .centerCrop()
                 .into(imageView);
+    }
+
+    public static Bitmap bitmapFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable background = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(40, 20, vectorDrawable.getIntrinsicWidth() + 40, vectorDrawable.getIntrinsicHeight() + 20);
+        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        background.draw(canvas);
+        vectorDrawable.draw(canvas);
+        return bitmap;
     }
 }
